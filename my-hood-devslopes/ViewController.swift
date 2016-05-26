db9de2d2b8d328b327cbf624d94a3606999c9d3d
Nam-Anh Vu <Nam-Anh@nam-anhs-macbook-pro.local>
2016-05-25 19:37:01 -0500

    Initial Commit

---
+++ my-hood-devslopes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-hood-devslopes
+//
+//  Created by Nam-Anh Vu on 5/25/16.
+//  Copyright © 2016 namdashann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
